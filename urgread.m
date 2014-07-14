dbf18ac2d582e8e300cc9c9b9626212b9349af19
BradleyClymer <brad.clymer@gmail.com>
2014-07-14 16:43:57 -0400

    Documentation and matching optimization
    Documenting code for presentation to Centosette, as well as tidying some
    things up that needed it.

--- urgread.m
+++ urgread.m
@@ -10,25 +10,37 @@ data_folder     = 'C:\Users\bclymer\Downloads'
 data_folder     = 'P:\Dropbox (Future Scan)\Flyswatter (1)\Testing'
 % data_folder     = old_folder
 
+%%  Input Parsing Block. 
+%   Asks for UrgBenri file, passes it to the file pre-processor, 
+%   'urg_struct_read'. This returns a struct containing date
+%   in text format and serial date number, the 1x1081 scan itself, the 
+%   header information, and a single date vector.
+%
+%   After that is returned, an IPD file is prompted. This is the timestamp
+%   from DPM, which is used to correlate URG time with transporter
+%   distance.
+
 if ~exist( 'urg_file' , 'var' )
     cd( data_folder )
-    [ fn , pn ]	= uigetfile( '*.ubh' , 'Select UrgBenri LiDAR data file' )
-    urg_file    = fullfile( pn , fn )
+    [ fn , pn ]	= uigetfile( '*.ubh' , 'Select UrgBenri LiDAR data file' )                  ;
+    urg_file    = fullfile( pn , fn )                                                       ;
+    fprintf( 'Urg file is:\n\t\t%s' , urg_file )
 end
-                  fclose( 'all' )
-fid             = fopen( urg_file )
+                  fclose( 'all' )                                                           ;
+fid             = fopen( urg_file )                                                         ;
 
 if ~exist( 'urg_struct' , 'var' ) 
-    disp( 'Reading Raw UrgBenri File' )
-    cd( old_folder )
-    urg_struct      = urg_struct_read( fid ) 
-    disp( 'Raw URG file has been read' )
+    disp( 'Reading Raw UrgBenri File' )                                                     
+    cd( old_folder )                                                                        ;
+    urg_struct      = urg_struct_read( fid )                                                ;
+
 end
 % 
 if ~exist( 'ipd_file' , 'var' )
     cd( data_folder )
-    [ fu , pu , iu ]	= uigetfile( fullfile( pn , '*.ipd' ) , 'Select HD index file' )
-    ipd_file            = fullfile( pu , fu )
+    [ fu , pu , iu ]	= uigetfile( fullfile( pn , '*.ipd' ) , 'Select HD index file' )    ;
+    ipd_file            = fullfile( pu , fu )                                               ;
+    fprintf( 'IPD file is:\n\t\t%s' , ipd_file )                        
 end
 
 if fu
@@ -49,9 +61,18 @@ end
 
                   fclose( 'all' )
 
+%%  Time and Distance correlation
+%   In this block, the timestamp in the urg file is correlated with that in
+%   the IPD file, from which an index is produced which generates a
+%   distance for each URG scan. The match is found in a least-squares
+%   sense. 
+
 urg_ts          = [ urg_struct.timeStamp ]'                                 ;
 ipd_ts          = [ ipd_struct.clock ]                                      ;
 disp( 'Matching distance indeces' )                             
+
+%   For a single value of the urg timestamp - 'urg_ts( i )' - subtract it
+%   from the IPD timestamp at all values. 
 
 for i = 1 : numel( urg_ts )
     abs_diff        = ( ipd_ts - urg_ts( i ) ) .^2                          ;
@@ -60,7 +81,9 @@ end
 
 urg_ft              = ipd_struct.ft( ipd_index )                            ;
 figure
-h.urg_distance      = plot( urg_ts - min( urg_ts ) , urg_ft )               ;
+h.urg_distance      = plot( urg_ts - min( urg_ts ) , urg_ft ,               ...
+                            'LineSmoothing' , 'on' ,                        ...
+                            'LineWidth' , 3 )                               ;
 title( urg_struct( 1 ).dateString )
 xt                  = get( get( h.urg_distance , 'Parent' ) , 'XTick' )     ;
 xtl                 = datestr( xt , 'MM:SS' )                               ;
@@ -69,9 +92,11 @@ xlabel( 'Time, minutes:seconds' )
 ylabel( 'Distance, feet ' )
 axis tight
 grid on
-
-% return
 force_filter_gen    = false                                                 ;
+
+%%  Generate or load low-pass FIR filter for corner detection.
+%   Low-pass filtering an individual scan takes out almost all features,
+%   except the hard corner where water reflection begins. 
 
 if ~exist( 'df.mat' , 'file' ) || force_filter_gen
 disp( 'Generating Spacial Filter' )
@@ -81,12 +106,14 @@ df               = designfilt(   'lowpassfir' ,                             ...
                                  'StopbandFrequency' ,       0.2 ,          ...
                                  'PassbandRipple' ,          1 ,            ...
                                  'StopbandAttenuation' ,     60 ,           ...
-                                 'SampleRate' ,              9 )         	;
-
-df
+                                 'SampleRate' ,              9 )            ;
 else
     load( 'df.mat' ) 
 end
+
+%%  Constants and settings
+%   Here we set overall parameters for the processing. 
+
 plot_parabola   = true                                                      ;
 run_calculations= true                                                      ;
 disp_plots      = true                                                      ;
@@ -123,11 +150,18 @@ filter_raw      = exp( filter_roll * ( filter_order : -1 : 1 )' /filter_order  )
 filter_new      = filter_raw / ( sum( filter_raw , 1 ) / 1 )                ;   % normalize coefficients
 filter_mat      = repmat( filter_new , 1 , 3 )                              ;   % matrix of coefficients
 min_rec         = zeros( numel( filter_order ) , 1 )                        ;   % initialize record of minima
+
+
+%%  Begin pre-processing of data.
+%   Nothing too novel. Remove scans with weird sizes first, initialize the
+%   filter block - to smooth the motion of centering, not the FIR filter
+%   above which finds corners in find_corners.m - as well as the curve
+%   which gets fit to the data and the record of vertices 
 struct_size_vec = cell2mat( arrayfun( @( x ) size( x.scan ) , urg_struct ,  ...
                            'UniformOutput' , false ) )                      ;   % size of scans. ex: 1081x1 
 bad_scans       = struct_size_vec( : , 1 ) ~= 1081                          ;   % find non-conforming scans
 urg_struct( bad_scans ) = []                                                ;   % remove bad scans
-num_scans       = size( urg_struct , 1  )
+num_scans       = size( urg_struct , 1  )                                   ;
 
 %   initialize stuff
 p               = zeros( num_scans , fit_order+1  )         ;
@@ -136,16 +170,29 @@ vertex          = zeros( num_scans, 2 )                     ;
 fit_range       = false( num_scans, 1081 )                  ;
 
 
-
+%%  Median filtering, outlier removal, geometric pre-pro
+%   Since the data is served up in vectors which always represent the same
+%   angles, a vector of the cosines of the corresponding angles gives the
+%   point-by-point weight of each value in the vector on the abscissa.
+%   Likewise, a vector of the sines of ( -45 : 0.25 : 225 ) gives the
+%   weight of each data vector in the ordinate axis. Here we have opted to
+%   pre-process the whole input data matrix by using repmat on the weight
+%   vectors, thereby taking advantage of MATLAB's often-faster-than-raw-C
+%   elementwise matrix multiplication speed, and cutdown on overhead during
+%   display.
+%
+%   In addition, here we strip out the noisy data points out at 60000mm ,
+%   and too far away to make sense. We also find the "initial offset," a
+%   guess of where the top of the pipe is from the middle 51 points of the
+%   input data.
 if ~exist( 'all_x_med' , 'var' )
     disp( 'Pre-Processing Data' )
-    all_x_weight    = repmat( x_weight , num_scans , 1 )                        ;   % cos( -45 : 0.25 : 1081 )
-    all_y_weight    = repmat( y_weight , num_scans , 1 )                        ;   % sin(  '       '     '  )
+    all_x_weight    = repmat( x_weight , num_scans , 1 )                        ;   % cos( -45 : 0.25 : 225 )
+    all_y_weight    = repmat( y_weight , num_scans , 1 )                        ;   % sin(  '      '     '  )
     sz              = size( all_x_weight )                                      ;   
 	raw_data        = double( horzcat( urg_struct.scan )' )                     ;
     infs            = ( raw_data == 60000 )                                     ;
     fars            = ( raw_data > ( 2 * pipe_diameter / mm_conversion ) )      ; 
-%     zeros           = ( raw_data == 0 )                                         ;
     raw_data( infs )= 0                                                         ;
     raw_data( fars )= 0                                                         ;
     middle_halo     = ( -20 : 20 ) + 1082/2                                     ;
@@ -178,12 +225,12 @@ if ~exist( 'all_x_med' , 'var' )
     disp( 'Median Filtering Complete.' )
     all_med( all_med == nan_value ) = nan                                       ;
     all_scans( all_scans == nan_value ) = nan                                   ;
-%     all_med( data.noise ) = nan                                                 ;
+%     all_med( data.noise ) = nan                                           	;
     all_x_med       = all_med .* all_x_weight                                   ;
     all_y_med       = all_med .* all_y_weight                                   ;
-    x_guess         = mean( nanmedian( data.x ) )
-    y_guess         = initial_offset - pipe_in
-    R_guess         = pipe_in 
+    x_guess         = mean( nanmedian( data.x ) )                               ;
+    y_guess         = initial_offset - pipe_in                                  ;
+    R_guess         = pipe_in                                                   ;
     par_rec         = repmat( [ x_guess y_guess R_guess ] ,                     ...
                       size( filter_mat , 1 ) , 1 )                              ;   % initialize record of circle parameters
     
